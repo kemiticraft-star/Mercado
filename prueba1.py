13bddbcddf6290816c2eaf78d4aa910c991305c8
kemiticraft-star <kemiticraft@gmail.com>
2026-02-05 08:30:47 -0500

    Add files via upload

---
+++ prueba1.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import openpyxl
+
+st.title("Mercado", text_alignment="center")
+
+# Leer Excel directamente desde el disco
+url = "https://docs.google.com/spreadsheets/d/1hfUF27WMtRIFtnqqgbZX3NU5FSlApzhnw2x3ALUDaVo/export?format=csv"
+
+df = pd.read_csv(url)
+
+st.subheader("Tabla original")
+st.dataframe(df)
+
+# Selección múltiple de índices
+indices = st.multiselect(
+    "Selecciona los índices",
+    sorted(df["Índice"].unique())
+)
+
+if indices:
+    filtrado = df[df["Índice"].isin(indices)]
+
+    resultado = (
+        filtrado
+        .groupby(["Unidad", "Producto"], as_index=False)["Cantidad"]
+        .sum()
+    )
+
+    st.subheader("Ingredientes totales")
+    st.dataframe(resultado)
+
+    st.subheader("Lista para comprar")
+    for _, fila in resultado.iterrows():
+        st.write(f'{fila["Cantidad"]} {fila["Unidad"]} {fila["Producto"]}')
